472ff8380881ac8e3445f4ab0e90a8307c236542
ShahriarHA <shahriar08neub@gmail.com>
2025-08-02 22:57:36 +0600

    Type Coversion: int to str

---
+++ Assignments/Python_115_problems/TypeConversion.py
@@ -0,0 +1,8 @@
+# Type Conversion: Given a list of integers, write a Python program to convert each element of the list to a string.
+IntegerList = [10,20,30,40,50,60]
+StringList = []
+for i in IntegerList:
+    # print(i)
+    StringList.append(str(i))
+print(StringList)
+
