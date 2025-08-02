5b6cf7412f81c088928cc930537e1e5619ced0d6
ShahriarHA <shahriar08neub@gmail.com>
2025-08-02 23:04:59 +0600

    Temperature Converter: C to F

---
+++ Assignments/Python_115_problems/TemperatureConverter.py
@@ -0,0 +1,6 @@
+# 5. Temperature Converter: Write a Python program that converts a temperature in Celsius to Fahrenheit. Take the Celsius temperature as input from the user.
+# °F = °C * 9/5 + 32
+celsiusTem = int(input())
+FahrenheitTem = celsiusTem * (9/5) + 32
+print(FahrenheitTem)
+
